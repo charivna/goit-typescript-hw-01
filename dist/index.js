60218edbfe572e03c2f267ad12a03fb8db9ea9c4
charivna <kate.dzhurinskaya@gmail.com>
2023-10-12 11:49:29 +0200

    push

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ import { concatenation } from "./concatenation";
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 if (button && input) {
-    button.addEventListener("click", () => {
-        concatenation(input.value, "hello!");
-    });
+  button.addEventListener("click", () => {
+    concatenation(input.value, "hello!");
+  });
 }
